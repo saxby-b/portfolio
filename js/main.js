f4aec776251491b59dac45235fce665b6668e373
C. Saxby Bain <csaxbybain@yahoo.com>
2025-11-08 14:18:17 -0500

    Added code to fix background color issue

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 const span = document.querySelector("span");
+const catRescue = document.querySelector(".cat-rescue");
+
 const d = new Date();
 const year = d.getFullYear();
 span.innerText = year;
+
+catRescue.style.background = "white";
+catRescue.contentWindow.document.style.body.backgroundColor = "white";
