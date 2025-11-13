797a3f795743740b1606ad12185edaff17334520
C. Saxby Bain <csaxbybain@yahoo.com>
2025-11-13 14:34:38 -0500

    Deleted unnecessary code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 const span = document.querySelector("span");
-const catRescue = document.querySelector(".cat-rescue");
 
 const d = new Date();
 const year = d.getFullYear();
 span.innerText = year;
-
-catRescue.style.background = "white";
-catRescue.contentWindow.document.style.body.backgroundColor = "white";
